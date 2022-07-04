5e82c200e95a04dc0b50298b15c72d0df81efb81
DanCan30 <danielecan3008@gmail.com>
2022-07-04 14:22:33 +0200

    Image selector added and text color changes

--- script/main.js
+++ script/main.js
@@ -5,6 +5,25 @@ const app = new Vue (
 
         data: {
             message: "",
+            colorClass: "",
+            imgSrc: "",
+            newImg: "",
+        },
+        
+        methods: {
+            changeImg: function() {
+                this.newImg = this.imgSrc;
+            },
+
+            applyEnphasis: function() {
+
+                if (this.message.endsWith("!")) {
+                    this.colorClass = "text-red";
+                } else {
+                    this.colorClass = "text-blue";
+                };
+                return this.colorClass;
+            },
         }
     }
 )
